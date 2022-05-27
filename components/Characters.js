dcf71194cecfba13f98a72475ac0a565184fa18c
unknown <ihernanhergert@gmail.com>
2022-05-27 01:44:48 -0300

    first commit

---
+++ components/Characters.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+const Characters = ({ characters = [] }) => {
+  return (
+    <div className="row">
+      {characters.map((item, index) => (
+        <div key={index} className="col">
+            <div className="card">
+                <Image src={item.image} alt="" width="400" height='400'></Image>
+                <h5>{item.name}</h5>
+                <p>Location: {item.location.name}</p>
+                <p>{item.spcies}</p>
+            </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Characters;
