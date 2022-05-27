b8e2276aca84b1993c5c3f990cbe91d94930a972
unknown <ihernanhergert@gmail.com>
2022-05-27 01:53:03 -0300

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link href="/">Rick & Morty</Link>
+      <Link href="/">Rick And Morty</Link>
     </div>
   );
 };
